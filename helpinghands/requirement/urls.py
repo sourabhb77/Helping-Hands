becb809dc78103218adbcd317e967ca74309c6d1
shubh <shubhambhakuni26@gmail.com>
2020-09-26 21:18:56 +0530

    Row distributin

--- helpinghands/requirement/urls.py
+++ helpinghands/requirement/urls.py
@@ -5,13 +5,11 @@ from . import views
 
 
 urlpatterns = [
-    path("" , views.listingListView.as_view(), name= "admin-home"),
-    path("home" , views.home, name= "home"),
-
+    path("" , views.listingListView.as_view(), name= "home"),
+    path("" , views.fillListForm , name ="logout"),
     path("new_listing" , views.fillListForm , name ="listing"),
     path("new_listings" , views.fillListForm , name ="login"),
     path("new_listings2" , views.fillListForm , name ="register"),
-    path("new_listings3" , views.fillListForm , name ="logout")
 ]
 
 
